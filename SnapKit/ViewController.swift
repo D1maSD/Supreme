afe0702a0a0dd567c252600334057af73cd74b04
Dima Melnik <melvin2k16@mail.ru>
2022-03-10 16:13:56 +0300

    second commit

--- SnapKit/ViewController.swift
+++ SnapKit/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var button = UIButton()
+        var num = 3
        // constraintSetup()
        
     }
